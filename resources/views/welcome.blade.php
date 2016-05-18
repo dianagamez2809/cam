59e15e20fbc56101203723e4b8beb7b2326cc86b
Diana Gamez <diana.gamez.fon@gmail.com>
2016-05-18 13:35:46 +0000

    Favicon working

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Laravel</title>
-        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+        <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}">
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
         <style>
